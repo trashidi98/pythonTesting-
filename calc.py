21bc2260adf38ec2dfa0abef4d7dcb6da6bb9f65
tabish98 <tabish.rashidi@gmail.com>
2019-04-01 19:08:06 -0400

    COMMIT 1

---
+++ calc.py
@@ -0,0 +1,17 @@
+
+#SAMPLE SCRIPT TO BE TESTED
+
+def add(x, y):
+    return x+y
+
+def sub(x, y):
+    return x-y
+
+def div(x,y):
+    if(y==0):
+        raise ValueError('CANT DIVIDE BY 0')
+    return x/y
+
+def mul(x, y):
+    return x*y
+
---
+++ test_calc.py
@@ -0,0 +1,17 @@
+import unittest
+import calc
+
+#SAMPLE TESTING SCRIPT 
+
+class TestClass(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(calc.add(10, 5), 15)
+
+    def test_sub(self):
+        self.assertEqual(calc.sub(10, 5), 5)
+
+    def test_div(self):
+        self.assertRaises(ValueError, calc.div, 10, 0)
+
+    def test_mul(self):
+        self.assertEqual(calc.mul(10, 5), 45)
